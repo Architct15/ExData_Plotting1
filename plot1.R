bf16e64938d79dd55a71fad6e3c0fb8c2c263698
Architct15 <chanhys@gmail.com>
2015-07-10 00:53:54 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,12 +1,45 @@
-# library(plyr)
+library(plyr)
 # setwd("course04")
-# rawdata<-read.table("household_power_consumption.txt", header=TRUE,sep=";")
-plotdata<-rawdata[rawdata$Date %in% c("1/2/2007", "2/2/2007"),]
+rawdata<-read.table("household_power_consumption.txt", stringsAsFactors=FALSE, header=TRUE,sep=";", na.strings = "?", 
+  colClasses=c(rep("character",2),rep("numeric",7)))
+plotdata<-subset(rawdata, rawdata$Date == "1/2/2007" | rawdata$Date == "2/2/2007")
 plotdata <- mutate(plotdata, datetime = as.POSIXlt(paste(as.Date(plotdata$Date, "%d/%m/%Y"), plotdata$Time)))
 View(plotdata)
-# plotdata$Date <- as.Date(plotdata$Date, "%m/%d/%y")
-# plotdata$Time <- strptime(plotdata$Time, "%H:%M:%S")
-# View(plotdata)
+# 1. Histogram
+windows(width=480, height=480)
+hist(plotdata$Global_active_power, col="red")
+
+# 2. Line chart
+# windows(width=480, height=480)
+# plot(plotdata$datetime, plotdata$Global_active_power, type="n")
+# lines(plotdata$datetime, plotdata$Global_active_power, col="black")
+
+# 3. Multiple line chart
+# windows(width=480, height=480)
+# with(plotdata, { plot(datetime, Sub_metering_1, type="n");
+# lines(datetime, Sub_metering_1, col="black");
+# lines(datetime, Sub_metering_2, col="red");
+# lines(datetime, Sub_metering_3, col="blue");
+# legend("topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+#        lty=c(1,1,1), lwd=c(2.5, 2.5, 2.5), col=c("black", "red", "blue"))})
+
+
+# 4. 2x2 plots
+# par(mfrow=c(2,2))
+# with(plotdata, { plot(datetime, Global_active_power, type="n");
+#       lines(datetime, Global_active_power, col="black")})
+# with(plotdata, { plot(datetime, Voltage, type="n");
+#        lines(datetime, Voltage, col="black")})
+# with(plotdata, { plot(datetime, Sub_metering_1, type="n");
+# lines(datetime, Sub_metering_1, col="black");
+# lines(datetime, Sub_metering_2, col="red");
+# lines(datetime, Sub_metering_3, col="blue");
+# legend("topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+#        lty=c(1,1,1), lwd=c(2.5, 2.5, 2.5), col=c("black", "red", "blue"), bty="n")})
+# with(plotdata, { plot(datetime, Global_reactive_power, type="n");
+#      lines(datetime, Global_active_power, col="black")})
+
+
 # with(plotdata, plot(x,y))
 # title(main = "main")
 # dev.copy(png, file = "xxx.png")
