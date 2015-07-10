804b455aed050b87422d22c6cc5bcbc7099d4e0c
Architct15 <chanhys@gmail.com>
2015-07-10 14:42:41 +0800

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,46 +1,35 @@
+# Project Name: Explorative Data Analysis Project 1
+# Part        : Plot 1 - Histogram for Global Active Power
+# Input Data  : downloaded and unzipped "household_power_consumption.txt 
+# Output Data : Global Active Power histogram in png format - "plot1.png"
+# Processing logic:
+# 1. Read raw data and assign data types
+# 2. Subset the data for selected 2 days
+# 3. Cleanse the data to get the right date-time for each row
+# 4. Open windows device and plot histogram
+# 5. Save plot to working directory
+
 library(plyr)
+
+# set working directory 
 # setwd("course04")
+
+# read the downloaded data file and assign proper class types (character or numberic)
 rawdata<-read.table("household_power_consumption.txt", stringsAsFactors=FALSE, header=TRUE,sep=";", na.strings = "?", 
-  colClasses=c(rep("character",2),rep("numeric",7)))
+                    colClasses=c(rep("character",2),rep("numeric",7)))
+
+# subset only the data for 2 days specified in the project 
 plotdata<-subset(rawdata, rawdata$Date == "1/2/2007" | rawdata$Date == "2/2/2007")
+
+# cleanse data by adding the right date-time for each row
 plotdata <- mutate(plotdata, datetime = as.POSIXlt(paste(as.Date(plotdata$Date, "%d/%m/%Y"), plotdata$Time)))
-View(plotdata)
-# 1. Histogram
-windows(width=480, height=480)
-hist(plotdata$Global_active_power, col="red")
 
-# 2. Line chart
-# windows(width=480, height=480)
-# plot(plotdata$datetime, plotdata$Global_active_power, type="n")
-# lines(plotdata$datetime, plotdata$Global_active_power, col="black")
+# open windows device and plot
+windows(width=480, height=480, bg="white")
+hist(plotdata$Global_active_power, col="red", 
+     main="Global Active Power", xlab = "Global Active Power (kilowatts)")
 
-# 3. Multiple line chart
-# windows(width=480, height=480)
-# with(plotdata, { plot(datetime, Sub_metering_1, type="n");
-# lines(datetime, Sub_metering_1, col="black");
-# lines(datetime, Sub_metering_2, col="red");
-# lines(datetime, Sub_metering_3, col="blue");
-# legend("topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-#        lty=c(1,1,1), lwd=c(2.5, 2.5, 2.5), col=c("black", "red", "blue"))})
+# Save plot to working directory
+dev.copy(png, file = "plot1.png")
+dev.off()
 
-
-# 4. 2x2 plots
-# par(mfrow=c(2,2))
-# with(plotdata, { plot(datetime, Global_active_power, type="n");
-#       lines(datetime, Global_active_power, col="black")})
-# with(plotdata, { plot(datetime, Voltage, type="n");
-#        lines(datetime, Voltage, col="black")})
-# with(plotdata, { plot(datetime, Sub_metering_1, type="n");
-# lines(datetime, Sub_metering_1, col="black");
-# lines(datetime, Sub_metering_2, col="red");
-# lines(datetime, Sub_metering_3, col="blue");
-# legend("topright",c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-#        lty=c(1,1,1), lwd=c(2.5, 2.5, 2.5), col=c("black", "red", "blue"), bty="n")})
-# with(plotdata, { plot(datetime, Global_reactive_power, type="n");
-#      lines(datetime, Global_active_power, col="black")})
-
-
-# with(plotdata, plot(x,y))
-# title(main = "main")
-# dev.copy(png, file = "xxx.png")
-# dev.off()
